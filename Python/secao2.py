401242973985c6e70a00a43684bd89e346e952e2
Deyved Antonio <deyved.antonio@gmail.com>
2022-03-31 22:20:56 -0300

    pratica de código.

---
+++ Python/secao2.py
@@ -0,0 +1,16 @@
+# Sintaxe básica
+print('Hello world!')
+
+# Variáveis
+a = 30
+b = 46
+
+# Operadores matemáticos
+soma = a + b
+subtracao = b - a
+divisao = b / a
+multiplicacao = a * b
+exponenciacao = a ** b
+modulo = a % b
+divisao_chao = b // a
+
