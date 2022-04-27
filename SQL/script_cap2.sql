fd1e2e6360492fa974ca5c8ea0194352ae9f960d
Deyved Antonio <deyved.antonio@gmail.com>
2022-04-26 21:07:51 -0300

    feat: finalização do dashboard para pratica dos conceitos

--- SQL/script_cap2.sql
+++ SQL/script_cap2.sql
@@ -1,3 +1,17 @@
 -- Criação do schema do capitulo 2
 CREATE SCHEMA `cap02` ;
 
+USE cap02;
+
+CREATE TABLE tb_navios
+(
+	nome_navio VARCHAR(50),
+    mes_ano VARCHAR(10),
+    classificacao_risco VARCHAR(15),
+    indice_conformidade VARCHAR(15),
+    pontuacao_risco INT,
+    temporada VARCHAR(200)
+);
+
+SELECT nome_navio, mes_ano FROM cap02.tb_navios;
+
