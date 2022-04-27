eb10d4174426a29a7216d4518964efc8cf7eea9a
Deyved Antonio <deyved.antonio@gmail.com>
2022-04-26 22:23:40 -0300

    test: exercitando conceitos cláusula where

--- SQL/script_cap2.sql
+++ SQL/script_cap2.sql
@@ -15,3 +15,17 @@ CREATE TABLE tb_navios
 
 SELECT nome_navio, mes_ano FROM cap02.tb_navios;
 
+SELECT * FROM cap02.tb_navios;
+
+SELECT nome_navio, mes_ano FROM cap02.tb_navios WHERE classificacao_risco = 'D';
+
+SELECT nome_navio, mes_ano FROM cap02.tb_navios WHERE classificacao_risco <> 'D';
+
+SELECT nome_navio, pontuacao_risco FROM cap02.tb_navios WHERE pontuacao_risco BETWEEN 50 AND 120;
+
+SELECT nome_navio, pontuacao_risco FROM cap02.tb_navios WHERE pontuacao_risco >= 50 AND pontuacao_risco <= 120;
+
+SELECT nome_navio, pontuacao_risco FROM cap02.tb_navios WHERE pontuacao_risco IN (50, 120); 
+
+SELECT nome_navio FROM cap02.tb_navios WHERE nome_navio LIKE 'A_A%';
+
