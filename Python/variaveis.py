ead52db6c6150fa65f09037b65020ecbab463957
Deyved Antonio <deyved.antonio@gmail.com>
2022-05-08 18:11:18 -0300

    test: teoria de código

---
+++ Python/variaveis.py
@@ -0,0 +1,14 @@
+# Variáveis
+
+idade = 26
+altura = 1.77
+nome = 'Deyved Antonio'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+linguagem = input('Qual é a linguagem de programação que você está estudando? ')
+print('A linguagem é', linguagem)
